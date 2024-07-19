b62622895b9ecb7f591fe2e1d429d2414ec99ec9
Scott Small <scott.small@semaphoresolutions.ca>
2024-07-19 10:49:25 -0700

    Release v1.6.0 (#62)
    * Updated version.py and changelog for 1.6.0 release
    
    * Declare support for Python 3.12 in setup.py (#60)
    
    * Fix EPP log message duplication and missing log entries in Clarity 6+ when combining Python automations with LLTK/LITKs (#51)
    
    * Fix EPP log message duplication and missing log entries in Clarity 6+ when combining Python automations with LLTK/LITKs
    
    * Fix current_minor_version failure on Python 2
    
    * StepRunner: ignore archived reagent kits when adding default reagents to a step (#53)
    
    * Fix EPP log message duplication and missing log entries in Clarity 6+ when combining Python automations with LLTK/LITKs
    
    * Fix current_minor_version failure on Python 2
    
    * StepRunner: ignore archived reagent kits when adding default reagents to a step
    
    * check for placement node that matches uri before creating one (#55)
    
    * check for placement node that matches uri before creating one
    
    * add missing quotes, fix for py2
    
    * remove f
    
    * Fix coveralls on Python 3.6 (#52)
    
    * Enable automated unit testing with Python 3.12 (#56)
    
    * Fix coveralls on Python 3.6
    
    * Enable automated unit testing with Python 3.12
    
    * Only trigger a production PyPi build on tags using semantic versioning (#57)
    
    * Use latest versions of GitHub workflow actions, build with Python 3.12 (#58)
    
    * Enable automated unit testing for Python 3.5 (#59)
    
    * Fix coveralls parallel builds (#61)
    
    ---------
    
    Co-authored-by: Scott Small <scott.small@semaphoresolutions.ca>
    
    ---------
    
    Co-authored-by: Joe Rickwalder <jrickwalder@billiontoone.com>

--- s4/clarity/steputils/step_runner.py
+++ s4/clarity/steputils/step_runner.py
@@ -340,10 +340,15 @@ class StepRunner:
         """
         For every required reagent kit in the step, the first active lot
         will be selected. If there are no active lots it will be omitted.
+
+        Archived kits will be ignored on Clarity 6.1 (api revision 32) and later.
         """
+        revision = int(self.lims.current_minor_version[1:])
         log.info("Adding default reagent lots.")
         lots = []
         for kit in self.step.configuration.required_reagent_kits:
+            if revision >= 32 and kit.archived:
+                continue
             for lot in kit.related_reagent_lots:
                 if lot.status == "ACTIVE":
                     lots.append(lot)
