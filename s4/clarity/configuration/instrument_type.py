06265db76033bb848fac82631d4365a39b262a81
Alan Lin <alin@billiontoone.com>
2025-10-14 10:18:18 -0700

    Changed InstrumentType comparison overloading typehints to docstrings to match python2

--- s4/clarity/configuration/instrument_type.py
+++ s4/clarity/configuration/instrument_type.py
@@ -4,7 +4,6 @@
 from s4.clarity._internal import ClarityElement
 from s4.clarity._internal.props import subnode_links, subnode_property
 from .process_type import ProcessType
-from typing import Union
 
 
 class InstrumentType(ClarityElement):
@@ -18,14 +17,36 @@ class InstrumentType(ClarityElement):
     vendor = subnode_property("vendor")
     process_types = subnode_links(ProcessType, "process-type", "process-types")
 
-    def __eq__(self, other: Union[str, ClarityElement, object]) -> bool:
+    def __eq__(self, other):
+        """comparison operator overloading,
+        If it's a string, check against the name, otherwise check against its URI.
+        This allows for backwards compatibility with the old `Instrument.instrument_type` which is a string.
+        An issue in v1.6.0 - in which the `Instrument.instrument_type` was a string.
+
+        Args:
+            other (str | ClarityElement | object): the other object to compare against
+
+        Returns:
+            bool: True if the instrument type is equal to the other object, False otherwise
+        """
         if isinstance(other, str):
             return self.name == other
         elif isinstance(other, ClarityElement):
             return self.uri == other.uri
         return False
 
-    def __ne__(self, other: Union[str, ClarityElement, object]) -> bool:
+    def __ne__(self, other):
+        """comparison operator overloading,
+        If it's a string, check against the name, otherwise check against its URI.
+        This allows for backwards compatibility with the old `Instrument.instrument_type` which is a string.
+        An issue in v1.6.0 - in which the `Instrument.instrument_type` was a string.
+
+        Args:
+            other (str | ClarityElement | object): the other object to compare against
+
+        Returns:
+            bool: True if the instrument type is not equal to the other object, False otherwise
+        """
         if isinstance(other, str):
             return self.name != other
         elif isinstance(other, ClarityElement):
