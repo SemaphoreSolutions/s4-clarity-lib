13d87398f7f039c0636e8ebe7eaf261a10bf37cf
Alan Lin <alin@billiontoone.com>
2025-10-23 08:55:09 -0700

    Bugfix: Add string comparison for InstrumentType (#85)
    * Bugfix: Add string comparison for InstrumentType
    
    In older versions of this s4 packages, example, v1.6.0, the Instrument.instrument_type is a string.
    So older s4 libraries does this sort of comparison:
    
    bravo.instrument_type == "bravo"
    
    But in the newer s4 packages, this comparison fails, because it's a string to object comparison. The user could update scripts, but something that is not feasible.
    
    Simple solution: set the comparison, when it is a string, to compare against its name, which is more intuitive. 
    
    This allows for backwards compatibility.
    If doing an object-to-object comparison, it still does a comparison against URI, which is a source of truth.
    
    * Update equality methods to use Union type hint instead of pipe character which was introduced in python v3.10
    
    * Changed InstrumentType comparison overloading typehints to docstrings to match python2

--- s4/clarity/configuration/instrument_type.py
+++ s4/clarity/configuration/instrument_type.py
@@ -16,3 +16,39 @@ class InstrumentType(ClarityElement):
     name = subnode_property("name")
     vendor = subnode_property("vendor")
     process_types = subnode_links(ProcessType, "process-type", "process-types")
+
+    def __eq__(self, other):
+        """comparison operator overloading,
+        If it's a string, check against the name, otherwise check against its URI.
+        This allows for backwards compatibility with the old `Instrument.instrument_type` which is a string.
+        An issue in v1.6.0 - in which the `Instrument.instrument_type` was a string.
+
+        Args:
+            other (str | ClarityElement | object): the other object to compare against
+
+        Returns:
+            bool: True if the instrument type is equal to the other object, False otherwise
+        """
+        if isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, ClarityElement):
+            return self.uri == other.uri
+        return False
+
+    def __ne__(self, other):
+        """comparison operator overloading,
+        If it's a string, check against the name, otherwise check against its URI.
+        This allows for backwards compatibility with the old `Instrument.instrument_type` which is a string.
+        An issue in v1.6.0 - in which the `Instrument.instrument_type` was a string.
+
+        Args:
+            other (str | ClarityElement | object): the other object to compare against
+
+        Returns:
+            bool: True if the instrument type is not equal to the other object, False otherwise
+        """
+        if isinstance(other, str):
+            return self.name != other
+        elif isinstance(other, ClarityElement):
+            return self.uri != other.uri
+        return True
